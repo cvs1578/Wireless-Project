f2f86735dcfa6a9497a3da7078b6a887a0db0373
cbasa97 <christianbasa09@gmail.com>
2021-04-11 22:31:22 -0500

    Add files via upload

--- Wireless2.m
+++ Wireless2.m
@@ -1,7 +1,9 @@
 %Generating node locations to help aid in the routing of a UAV
 %Genuine Nodes represent refill stations
 %Malicious Nodes represent areas to avoid
-
+clc;
+clear all;
+close all;
 no_nodes = input('Enter the number of nodes: ');
 no_nodes_m = input('Enter the number of malicious nodes: ');
 network_length = input('Enter the length of the network: ');
@@ -10,46 +12,48 @@ network_width = input('Enter the width of the network: ');
 for i = 1:no_nodes
     x_loc(i) = network_length*rand;
     y_loc(i) = network_width*rand;
-    nodes_id(i) = i; 
+    %nodes_id(i) = i; 
     plot(x_loc(i),y_loc(i),'b^','linestyle','none')
     hold on
     xlabel('Network Length');
     ylabel('Network Height');
     grid on
-    pause(1.0);
+    pause(.5);
 end
 for j = 1:no_nodes_m
     x_loc_m(j) = network_length*rand;
     y_loc_m(j) = network_width*rand;
-    nodes_id_m(j) = j; 
+    %nodes_id_m(j) = j; 
     plot(x_loc_m(j),y_loc_m(j),'ro','linestyle','none')
     hold on;
     xlabel('Network Length');
     ylabel('Network Height');
     grid on
-    pause(1.0);
+    pause(.5);
 end
 
-%Arbitarilly choosing source and destination randomly.
-source = rem(round((no_nodes+no_nodes_m)*rand*3),no_nodes);
+source = rem(round((no_nodes+no_nodes_m)*rand),no_nodes);
 if source == 0
-    source = 5;
+    source = 15;
 end
 destination = rem(round((no_nodes+no_nodes_m)*rand),no_nodes);
 if destination == 0
-    destination = 6;
+    destination = 16;
 end
-
 figure(1)
-plot(x_loc(source),y_loc(source),'g^','linestyle','none')
+plot(x_loc(source),y_loc(source),'b^','linewidth',2);
 text(x_loc(source),y_loc(source), 'SRC')
+
 hold on
-plot(x_loc(destination),y_loc(destination),'g^','linestyle','none')
+plot(x_loc(destination),y_loc(destination),'g^','linewidth',2);
 text(x_loc(destination),y_loc(destination), 'Destination')
 
 
-
-
+source_loc = [x_loc(source), y_loc(source)];
+destination_loc = [x_loc(destination), y_loc(destination)];
+%p1 = [x_loc(source) x_loc_m(destination)];
+%p2 = [y_loc(source) y_loc_m(destination)];
+%plot([p1(1) p2(1)],[p1(2) p2(2)])
 
 
 
